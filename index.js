2a03e01a1ba585d83cde7b11ecac56b777b4fca6
João Vitor <joaovitoraraujo1821@gmail.com>
2022-06-15 20:41:15 -0300

    Terminie a lógica do programa

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
 const main = () => {
-    const prompt = require('prompt-sync')();
     const words = [];
+    const prompt = require('prompt-sync')();
+
+    console.log('-'.repeat(35));
+    console.log('Liste as propriedades CSS desejadas');
+    console.log(`Digite SAIR para encerrar o programa`);
+    console.log('-'.repeat(35));
 
     while (true) {
-        const word = prompt('Digite uma propriedade CSS : ');
-       
-        if (word == 'SAIR') {
-            break;
 
-        } else {
-            words.push(word);
+        try {
+            const word = prompt('>>>> ').toLowerCase();
+            if (word == 'sair') {
+                break;
+    
+            } else {
+                words.push(word);
+            };
+
+        } catch (error) {
+            console.log(error);
+            continue;
         }
+       
+    };
+    console.log('-'.repeat(30));
+    console.log('    Lista de propriedades');
+    
+
+    for (word of words.sort()) {
+        console.log(`${words.indexOf(word) + 1} - ${word}`);
     };
 
-    console.log(words)
+    console.log('-'.repeat(30));
 };
 
 main();
